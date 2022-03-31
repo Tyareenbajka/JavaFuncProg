70babbd1abf3d6e787086840dc7979890ce597a4
Philip Tonaczew <58476139+Tyareenbajka@users.noreply.github.com>
2022-03-31 22:31:55 +0200

    Update Tomteland.kt

--- src/seminar2/exercise1/Tomteland.kt
+++ src/seminar2/exercise1/Tomteland.kt
@@ -2,26 +2,8 @@ package seminar2.exercise1
 
 /*
 
-Philip Tonaczew
+Philip Tonaczew 2022-01-18 
 
-Tomtarna på Nordpolen har en strikt chefs-hierarki
-
-Högsta chefen för allt är "Tomten"
-Under "Tomten" jobbar "Glader" och "Butter"
-Under "Glader" jobbar "Tröger", "Trötter"och "Blyger"
-Under "Butter" jobbar "Rådjuret", "Nyckelpigan", "Haren" och "Räven"
-Under "Trötter" jobbar "Skumtomten"
-Under "Skumtomten" jobbar "Dammråttan"
-Under "Räven" jobbar "Gråsuggan" och "Myran"
-Under "Myran" jobbar "Bladlusen"
-
-Er uppgift är att illustrera tomtens arbetshierarki i en lämplig datastruktur.
-(Det finns flera tänkbara datastrukturer här)
-Skriv sedan en funktion där man anger ett namn på tomten eller någon av hens underhuggare som
-inparameter.
-Funktionen listar sedan namnen på alla underlydandesom en given person har
-Expempel: Du anger "Trötter" och får som svar ["Skumtomten", "Dammråttan"]"
-För att bli godkänd på uppgiften måste du använda rekursion.
  */
 
 class Tomteland {
@@ -36,10 +18,7 @@ class Tomteland {
         "Myran" to listOf("Bladlusen")
     )
 
-    // current namn är den tomte vars underlydande ni vill ta fram
-    //res är listan som håller alla underlydande
     fun getUnderlings(currentName: String, res: MutableList<String>): List<String> {
-
         fun underlings(list: List<String>): List<String> {
             for (s in list) {
                 res.add(s)
@@ -52,10 +31,6 @@ class Tomteland {
 }
 
 fun main() {
-    //Exempel på anrop till den rekursiva funktionen getUnderlings,
-    // här är tanken att hitta Gladers underlydande
-    //listan fylls på successivt när vi rekurserar
-
     val tomteland = Tomteland()
     val list: MutableList<String> = mutableListOf()
 
